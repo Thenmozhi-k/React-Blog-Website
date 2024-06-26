edecd70e5459ed6c00c2125fb79893d054992bb3
Thenmozhi <jothi@gmail.com>
2024-06-27 00:36:41 +0530

    first commit

--- src/Components/BlogPage.jsx
+++ src/Components/BlogPage.jsx
@@ -1,11 +1,21 @@
 import React, { useEffect, useState } from 'react';
+import BlogCard from './BlogCard';
 
 const BlogPage = () => {
   const [blogs, setBlogs] = useState([]);
+  const [currentPage , setCurrentPage] = useState(1)
+  const pageSize = 12;
+  const [selectedCategory , setSelectedCategory] = useState(null);
+  const [activeCategory , setActiveCategory] = useState(null);
+
 
   useEffect(() => {
     async function fetchBlogs() {
-      const url = 'http://localhost:5000/blogs';
+      const url = `http://localhost:5000/blogs?page=${currentPage} & limit=${pageSize}`;
+
+      if(selectedCategory) {
+        url += `&category=${selectedCategory}`
+      }
 
       try {
         const response = await fetch(url);
@@ -21,17 +31,35 @@ const BlogPage = () => {
     }
 
     fetchBlogs();
-  }, []);
+  }, [currentPage, pageSize, selectedCategory]);
+
+  const handlePageChange = (category) => {
+    setCurrentPage(category)
+  }
+
+  const handleCategoryChange = (category) => {
+    setSelectedCategory(category);
+    setCurrentPage(1);
+    setActiveCategory(category)
+
+  }
 
   return (
     <div>
-      {blogs.map(blog => (
+      {/* {blogs.map(blog => (
         <div key={blog.id}>
           <h2>{blog.title}</h2>
           <img src={blog.image} alt={blog.title} />
           <p>{blog.content}</p>
         </div>
-      ))}
+      ))} */}
+      <div>Page Category</div>
+
+      {/* blogcards section */}
+      <div><BlogCard blogs={blogs} currentPage={currentPage} pageSize={pageSize} selectedCategory={selectedCategory}/></div>
+
+      {/* pagination section */}
+      <div>Pagination</div>
     </div>
   );
 };
