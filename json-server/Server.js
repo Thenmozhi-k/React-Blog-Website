789f7522c4eac96bbfc86743e1c7a4185b4a3912
Thenmozhi <jothi@gmail.com>
2024-06-26 19:35:00 +0530

    first commit

---
+++ json-server/Server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.get('/blogs', (req, res) => {
+    const filePath = path.join(__dirname, 'blogsData.json');
+    console.log(`Serving file from: ${filePath}`); // Debug log
+    res.sendFile(filePath, (err) => {
+        if (err) {
+            console.error('Error sending file:', err);
+            res.status(err.status).end();
+        } else {
+            console.log('File sent:', filePath);
+        }
+    });
+});
+
+const port = 5000;
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
--- src/Components/BlogPage.jsx
+++ src/Components/BlogPage.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
 const BlogPage = () => {
-  return (
-    <div>BlogPage</div>
-  )
-}
+  const [blogs, setBlogs] = useState([]);
 
-export default BlogPage
+  useEffect(() => {
+    async function fetchBlogs() {
+      const url = 'http://localhost:5000/blogs';
+
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        console.log(data);
+        setBlogs(data);
+      } catch (error) {
+        console.error('Fetch error:', error);
+      }
+    }
+
+    fetchBlogs();
+  }, []);
+
+  return (
+    <div>
+      {blogs.map(blog => (
+        <div key={blog.id}>
+          <h2>{blog.title}</h2>
+          <img src={blog.image} alt={blog.title} />
+          <p>{blog.content}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BlogPage;
