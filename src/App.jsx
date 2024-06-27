6b5b0587cfa135b3ade02aec9594087ac00fb821
Thenmozhi <jothi@gmail.com>
2024-06-27 23:11:57 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
 import Nav from './Components/Nav'
+import Footer from './Components/Footer'
 
 function App() {
  
@@ -10,6 +11,7 @@ function App() {
     <>
       <Nav/>
       <Outlet/>
+      <Footer/>
     </>
   )
 }
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,120 @@
 import React from 'react'
+import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa6'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='bg-gray-900'>
+        <div className='px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-4'>
+            <div className='grid mb-8 lg:grid-cols-6'>
+                 <div className='grid grid-cols-2 gap-5 lg:col-span-4 md:grid-cols-4'>
+                    <div>
+                        <p className='font-medium tracking-wide text-gray-300'>Category</p>
+                        <ul className='mt-2 space-y-2'>
+                            <li>
+                                <a href="/" className='text-gray-500 transition-colors duration-300 hover:text-orange-500 '>News</a>
+                            </li>
+                            <li>
+                                <a href="/" className='text-gray-500 transition-colors duration-300 hover:text-orange-500 '>World</a>
+                            </li>
+                            <li>
+                                <a href="/" className='text-gray-500 transition-colors duration-300 hover:text-orange-500 '>Games</a>
+                            </li>
+                            <li>
+                                <a href="/" className='text-gray-500 transition-colors duration-300 hover:text-orange-500 '>Refrences</a>
+                            </li>
+                        </ul>
+                    </div>
+                    {/* category 2 */}
+
+                    <div>
+                        <p className='font-medium tracking-wide text-gray-300'>Apples</p>
+                        <ul className='mt-2 space-y-2'>
+                            <li>
+                                <a href="/" className='text-gray-500 transition-colors duration-300 hover:text-orange-500 '>Web</a>
+                            </li>
+                            <li>
+                                <a href="/" className='text-gray-500 transition-colors duration-300 hover:text-orange-500 '>E-Commerce</a>
+                            </li>
+                            <li>
+                                <a href="/" className='text-gray-500 transition-colors duration-300 hover:text-orange-500 '>Business</a>
+                            </li>
+                            <li>
+                                <a href="/" className='text-gray-500 transition-colors duration-300 hover:text-orange-500 '>Entertainment</a>
+                            </li>
+                            <li>
+                                <a href="/" className='text-gray-500 transition-colors duration-300 hover:text-orange-500 '>Portfolio</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    {/* category 3 */}
+                    <div>
+                        <p className='font-medium tracking-wide text-gray-300'>Cherry</p>
+                        <ul className='mt-2 space-y-2'>
+                            <li>
+                                <a href="/" className='text-gray-500 transition-colors duration-300 hover:text-orange-500 '>Media</a>
+                            </li>
+                            <li>
+                                <a href="/" className='text-gray-500 transition-colors duration-300 hover:text-orange-500 '>Broucher</a>
+                            </li>
+                            <li>
+                                <a href="/" className='text-gray-500 transition-colors duration-300 hover:text-orange-500 '>Nonprofit</a>
+                            </li>
+                            <li>
+                                <a href="/" className='text-gray-500 transition-colors duration-300 hover:text-orange-500 '>Education</a>
+                            </li>
+                            <li>
+                                <a href="/" className='text-gray-500 transition-colors duration-300 hover:text-orange-500 '>Projects</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                      {/* category 4 */}
+                      <div>
+                        <p className='font-medium tracking-wide text-gray-300'>Business</p>
+                        <ul className='mt-2 space-y-2'>
+                            <li>
+                                <a href="/" className='text-gray-500 transition-colors duration-300 hover:text-orange-500 '>Infopreneur</a>
+                            </li>
+                            <li>
+                                <a href="/" className='text-gray-500 transition-colors duration-300 hover:text-orange-500 '>Personal</a>
+                            </li>
+                            <li>
+                                <a href="/" className='text-gray-500 transition-colors duration-300 hover:text-orange-500 '>Wiki</a>
+                            </li>
+                            <li>
+                                <a href="/" className='text-gray-500 transition-colors duration-300 hover:text-orange-500 '>Forum</a>
+                            </li>
+                            <li>
+                                <a href="/" className='text-gray-500 transition-colors duration-300 hover:text-orange-500 '>Projects</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                   
+                 </div>
+
+                  {/* subcription*/}
+                  <div className='md:max-w-md lg:col-span-2 lg:mt-0 mt-5'>
+                        <p className='font-medium tracking-wide text-gray-300'>Subscrption for updates</p>
+                        <form className='mt-4 flex flex-col md:flex-row ' >
+                            <input type="email" name='email' id='email' className='flex-grow w-full h-12 px-4 mb-3 transition duration-200 bg-white border border-gray-300 rounded shadow-sm aspect-auto md:mr-2 focus:border-purple-400 focus:outline-none' />
+                            <button className='inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md hover:bg-orange-500 focus:outline-none border' type='submit'>Subscribe</button>
+                        </form>
+                        <p className='mt-4 text-sm text-gray-500'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis exercitationem, modi quos eligendi nostrum similique vel sequi dolorum placeat voluptatibus expedita quibusdam ex libero ut, iure dicta adipisci consectetur. Ducimus?</p>
+                    </div>
+            </div>
+
+             <div>
+                <p>Copyright 2024 | All rigth reserved.</p>
+                <div>
+                    <a className='text-gray-500 transition-all duration-300 hover:text-orange-500' href=""><FaTwitter className='h-6 w-6' /></a>
+                    <a className='text-gray-500 transition-all duration-300 hover:text-orange-500' href=""><FaInstagram className='h-6 w-6' /></a>
+                    <a className='text-gray-500 transition-all duration-300 hover:text-orange-500' href=""><FaFacebook className='h-6 w-6' /></a>
+                </div>
+             </div>
+        </div>
+    </div>
   )
 }
 
